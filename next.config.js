6da7bcb6ee1ddaa0d5cec7990b4cc200bf8de959
raedmokni <raedmokni2@gmail.com>
2025-03-17 14:11:49 +0100

    mdlware debug

--- next.config.js
+++ next.config.js
@@ -8,6 +8,15 @@ const nextConfig = {
     ignoreBuildErrors: true, // ✅ Allows the build to succeed even with TypeScript errors
   },
   images: { unoptimized: true },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL + '/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
